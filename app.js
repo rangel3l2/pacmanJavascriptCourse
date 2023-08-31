e8798eaebaab95e35f48824889fee2693e01b977
rangel3l2 <rangel.silva@estudante.ifms.edu.br>
2023-08-31 03:39:30 -0400

    createboard upd

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 
-   const container = document.querySelector('.container')
+   const container = document.querySelector('.container')   
    const grid = document.createElement('div')
+    grid.classList.add('grid')
+   
+   squares = []
    const layout = [
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
     1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
@@ -31,9 +34,37 @@
     1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
     1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
   ]
-  // 0 - pac-dots
+  // 0 - pac-dot
   // 1 - wall
   // 2 - ghost-lair
   // 3 - power-pellet
   // 4 - empty
 
+function buildBoard(){
+  container.appendChild(grid)
+  for(let i=0;i<layout.length;i++){
+    const square = document.createElement('div')
+    grid.appendChild(square)
+    squares.push(square)
+    switch(layout[i]){
+      case 0 :
+        square.classList.add('pac-dot')
+        break    
+      case 1 :
+        square.classList.add('wall')
+        break
+      case 2 :
+        square.classList.add('ghost-lair')
+        break
+      case 3 :
+        square.classList.add('power-pellet')
+        break
+      case 4 :
+        square.classList.add('empty')
+        break
+
+    }
+  }
+
+}
+buildBoard()
